71fcd171ec4be7daab515ad0eedacfb898fc23cd
“verbol” <verbol96@mail.ru>
2023-02-09 10:26:22 +0300

    2

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use('/api', router)
 
 const start = async() =>{
     try {
-        await sequelize.authenticate()
-        await sequelize.sync()
+        //await sequelize.authenticate()
+        //await sequelize.sync()
         app.listen(PORT, ()=> console.log(`server started! ${PORT}`))
     } catch (error) {
         console.log(error)
